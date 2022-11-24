430afc2537b86f1a1e19b302027641338dafe7f4
yuzumone <yuzumone@users.noreply.github.com>
2022-11-24 18:38:10 +0900

    Add sbi (#4)

--- kurashi/cli.py
+++ kurashi/cli.py
@@ -2,6 +2,7 @@ import click
 from kurashi.tepco import tepco
 from kurashi.water import water
 from kurashi.aeon_bank import aeon_bank
+from kurashi.sbi import sbi
 
 
 @click.group(context_settings=dict(help_option_names=['-h', '--help']))
@@ -12,6 +13,7 @@ def main():
 main.add_command(tepco)
 main.add_command(water)
 main.add_command(aeon_bank)
+main.add_command(sbi)
 
 
 if __name__ == '__main__':
